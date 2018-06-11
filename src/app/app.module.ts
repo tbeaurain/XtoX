48e1bed95385b8a8523e074cf9508b72593e143f
tbeaurain <thibaut@ThibautBeaurain.local>
2018-06-11 10:08:24 +0200

    add functionality file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './webPage/nav-bar/nav-bar.component';
+import { NavBarComponent } from './functionality/nav-bar/nav-bar.component';
 import { HomeComponent } from './webPage/home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { ProfilComponent } from './webPage/profil/profil.component';
-import { SearchBarComponent } from './webPage/search-bar/search-bar.component';
+import { SearchBarComponent } from './functionality/search-bar/search-bar.component';
 import { ResultComponent } from './webPage/result/result.component';
 
 
