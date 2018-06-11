12ab45ab0363f8fd134d5fc15bf8fadcfaae02ee
tbeaurain <thibaut@ThibautBeaurain.local>
2018-06-11 12:14:35 +0200

    change file

--- src/app/functionality/users/users.component.ts
+++ src/app/functionality/users/users.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../../Class/user';
+import { User } from '../../class/user';
 import { UserService } from './user.service';
 
 @Component({
