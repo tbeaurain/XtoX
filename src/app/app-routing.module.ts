6b00924f44dec48dcb689142f823a361b8c27f8b
Eliemimi <emilie.mignot28@gmail.com>
2018-06-11 11:07:21 +0200

    modif users

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-import { HomeComponent } from './home/home.component';
-import { ProfilComponent } from './profil/profil.component';
-import { ResultComponent } from './result/result.component';
-import { UsersComponent } from './users/users.component';
-=======
+
 import { HomeComponent } from './webPage/home/home.component';
 import { ProfilComponent } from './webPage/profil/profil.component';
 import { ResultComponent } from './webPage/result/result.component';
->>>>>>> 48e1bed95385b8a8523e074cf9508b72593e143f
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'profil', component: ProfilComponent },
   { path: 'result', component: ResultComponent },
-  { path: 'user', component: UsersComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,12 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './functionality/nav-bar/nav-bar.component';
 import { HomeComponent } from './webPage/home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
-<<<<<<< HEAD
-import { ProfilComponent } from './profil/profil.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
-import { ResultComponent } from './result/result.component';
-import { UsersComponent } from './users/users.component';
-=======
+
 import { ProfilComponent } from './webPage/profil/profil.component';
 import { SearchBarComponent } from './functionality/search-bar/search-bar.component';
 import { ResultComponent } from './webPage/result/result.component';
->>>>>>> 48e1bed95385b8a8523e074cf9508b72593e143f
+import { UsersComponent } from './functionality/users/users.component';
+
 
 
 @NgModule({
